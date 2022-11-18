ab0af2c09a981af383a34d2abe59f3e8cd232f10
Daniel Alexander Dam <dani36i9@stud.kea.dk>
2022-11-18 14:28:49 +0100

    initial scaffolding

---
+++ src/main/java/com/example/bilabonnement/BilabonnementApplication.java
@@ -0,0 +1,13 @@
+package com.example.bilabonnement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilabonnementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BilabonnementApplication.class, args);
+    }
+
+}
