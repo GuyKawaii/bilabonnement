f1b0fb4ab45dd432f959942eb9c08e4381cc97d9
Daniel Alexander Dam <dani36i9@stud.kea.dk>
2022-11-25 14:44:15 +0100

    add mapping for carLeased and income

--- src/main/java/com/example/bilabonnement/controller/BusinessDeveloperController.java
+++ src/main/java/com/example/bilabonnement/controller/BusinessDeveloperController.java
@@ -1,8 +1,34 @@
 package com.example.bilabonnement.controller;
 
+
+import com.example.bilabonnement.model.Car;
+import com.example.bilabonnement.service.FleetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpSession;
 import java.sql.Date;
+import java.util.List;
 
 @Controller
 public class BusinessDeveloperController {
+    FleetService fleetService = new FleetService();
+    @GetMapping("carsLeased")
+    public String getCarsLeased(HttpSession session, Model model) {
+       List<Car> leasedCars = fleetService.getLeasedCarsList();
+       int numOfLeasedCars = fleetService.getLeasedCarsAmount();
+
+       model.addAttribute("leasedCars",leasedCars);
+       model.addAttribute("numberOfLeasedCars",numOfLeasedCars);
+
+        return "business-developer";
+    }
+
+    @GetMapping("income")
+    public String showIncome() {
+
+        return "incomeData";
+    }
+
 }
