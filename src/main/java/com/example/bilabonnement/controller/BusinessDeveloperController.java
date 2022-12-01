a58cfba81f8d0c43bab20d8fb3ada684516209ad
CodeClod <mika9103@stud.kea.dk>
2022-12-01 12:18:58 +0100

    Frontend work on business-developer site

--- src/main/java/com/example/bilabonnement/controller/BusinessDeveloperController.java
+++ src/main/java/com/example/bilabonnement/controller/BusinessDeveloperController.java
@@ -14,11 +14,16 @@ import java.util.List;
 @Controller
 public class BusinessDeveloperController {
     FleetService fleetService = new FleetService();
-    @GetMapping("carsLeased")
+    @GetMapping("finance")
     public String getCarsLeased(HttpSession session, Model model) {
        List<Car> leasedCars = fleetService.getLeasedCarsList();
        int numOfLeasedCars = fleetService.getLeasedCarsAmount();
 
+       // TODO Fix this after asking for definitive session/cookie usage help
+        // Blocked 'cause of the undefined
+       // String nameOfUser = session.getAttribute(#);
+
+       model.addAttribute("nameOfUser",session.getAttribute("name"));
        model.addAttribute("leasedCars",leasedCars);
        model.addAttribute("numberOfLeasedCars",numOfLeasedCars);
 
